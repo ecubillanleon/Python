88e1361619ebb646d6715f8230fdbaecf188642a
Enmanuel Cubillan León <enmanuelcubillan@gmail.com>
2017-05-01 14:58:50 -0400

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,32 @@
+#Autor: Enmanuel Cubillan
+a = 26
+b = 11.5
+c = 5
+d = 3.5
+
+# Suma
+print "La suma es: ",a+b
+
+#Resta
+print "La resta es: ",c-a
+
+#Multiplicacion
+print "La Multiplicacion es: ",d*c
+
+#Exponente
+print "El exponente es: ",c**2
+
+#Division
+print "La division es: ",b/a
+
+#Division entera
+print "La division entera es:",7/3
+
+#Modulo
+print "El modulo es: ",7%3
+
+
+
+
+
+
