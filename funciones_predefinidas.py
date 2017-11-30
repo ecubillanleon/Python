4872f9094f8ce0c0dd50abd392ce9d8047020bd1
Enmanuel Cubillan <enmanuelcubillan@fec.luz.edu.ve>
2017-11-30 15:40:50 -0400

    Algunas funciones predefinidas en Python

---
+++ funciones_predefinidas.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Autor: Enmanuel Cubillan
+# Funciones predefinidas en Python
+
+print 'Funciones predefinidas en Python'
+print '\nMuestra el código ascii'
+
+""" La función chr convierte un código a un caracter de la tabla ascii """
+print 'El código 97 convertido es: ' + str(chr(97))
+
+""" La función ord convierte un caracter de la tabla ascii
+a un código alfanumerico """
+print 'El código (a) convertido es: ' +str(ord('a'))
+
+print '\nValor absoluto: ' +str(abs(-45))
+print 'Número flotante: ' +str(float(100))
+print 'Número entero: ' +str(int(3.1415))
+print 'Cadena: ' +str('4587')
+print 'Redondeo hacia bajo: ' +str(round(45.25))
+print 'Redondeo hacia arriba: ' +str(round(45.75))
+print 'Redondeo: ' +str(round(1.123,5))
